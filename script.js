daf723f5a187bd513bcd49b2608826942ec0c62a
hugovk <hugovk@users.noreply.github.com>
2016-05-02 15:44:47 +0300

    Auto-indent lines

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function getDistanceFromLatLonInMetres(lat1, lon1, lat2, lon2) {
   var dLat = deg2rad(lat2-lat1);  // deg2rad below
   var dLon = deg2rad(lon2-lon1);
   var a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
     ;
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
   var d = R * c; // Distance in km
@@ -40,7 +40,7 @@ function lookupLocation() {
 }
 
 function numberWithSpaces(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "&nbsp;");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "&nbsp;");
 }
 
 function ShowClosest(loc) {
@@ -92,8 +92,8 @@ $(document).ready(function() {
   // Boot up the satellites
   if (geoPosition.init()) {
     $("#live-geolocation").html('Checking...');
-      lookupLocation();
-    } else {
+    lookupLocation();
+  } else {
     $("#live-geolocation").html('Dunno.');
   }
 });
