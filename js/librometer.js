fcbdb59118a7d556b0fa034d957c1f14ebd00e37
Hugo <hugovk@users.noreply.github.com>
2019-01-16 20:50:16 +0200

    Librometer: How far to the nearest open Finnish library?

---
+++ js/librometer.js
@@ -0,0 +1,37 @@
+async function ShowClosest(loc) {
+
+  const URL = 'https://api.kirjastot.fi/v4/library?status=open&geo.pos=' + loc.coords.latitude + ',' + loc.coords.longitude;
+  console.log(URL)
+
+  const response = await fetch(URL);
+  const json = await response.json();
+
+  libraries = json.items;
+
+  // Reset list
+  $("#live-geolocation").html('Closest open:');
+  $("ul").empty();
+
+  // Update list
+  $.each(libraries, function (key, val) {
+    console.log(val)
+    $('#metro-list').append(
+      $('<li class="station">').append(val.name + ' <span class="dist">' + numberWithSpaces(val.distance * 1000) + '&nbsp;m</span>'));
+  });
+
+}
+
+function ShowClosestError() {
+  $("#live-geolocation").html('Dunno closest.');
+}
+
+$(document).ready(function () {
+
+  // Boot up the satellites
+  if (geoPosition.init()) {
+    $("#live-geolocation").html('Checking...');
+    lookupLocation();
+  } else {
+    $("#live-geolocation").html('Dunno.');
+  }
+});
